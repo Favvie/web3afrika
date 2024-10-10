287b415582c508e6f48e8c33a8da634502b43131
Favvie <favourabangwu@gmail.com>
2024-10-10 23:16:09 +0100

    feat: add features

---
+++ frontend/src/app/contribute/page.tsx
@@ -0,0 +1,161 @@
+// import { useState } from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Web3 from "web3";
+import { useRouter } from "next/navigation";
+import { ethers } from "ethers";
+
+import { TOKEN_CONTRACT_ABI } from "@/config/TokenContract";
+import { CAUSEKOIN_CONTRACT_ABI } from "@/config/CauseKoin";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+export default function Home() {
+	// const [currentAccount, setCurrentAccount] = useState<string | null>(null);
+	const [tokenBalance, setTokenBalance] = useState<number>(0);
+	const [walletAddress, setWalletAddress] = useState<string>("");
+	const router = useRouter();
+
+	// Initialize Web3 instance
+	const web3 = window.ethereum ? new Web3(window.ethereum) : null;
+
+	const getTokenBalance = async () => {
+		try {
+			const { ethereum } = window;
+			if (walletAddress && ethereum) {
+				const provider = new ethers.BrowserProvider(ethereum);
+				const signer = await provider.getSigner();
+
+				const tokenContract = new ethers.Contract(process.env.NEXT_PUBLIC_CAUSEKOIN_CONTRACT_ADDRESS as string, CAUSEKOIN_CONTRACT_ABI, signer);
+
+				// Call contractTokenBalance method to get the token balance
+				const balance = await tokenContract.contractTokenBalance({ gasLimit: 300000 });
+				const formattedBalance = ethers.formatUnits(balance, 18); // Assuming 18 decimals for the token
+				console.log("Contract Token balance:", formattedBalance);
+
+				setTokenBalance(parseFloat(formattedBalance));
+			} else {
+				console.log("No wallet address or Ethereum provider found");
+			}
+		} catch (error) {
+			console.log("Error fetching token balance:", error);
+		}
+	};
+
+	const requestAccount = async (): Promise<void> => {
+		console.log("Requesting account...");
+		if (window.ethereum) {
+			try {
+				console.log("MetaMask detected");
+				const accounts: string[] = await window.ethereum.request({
+					method: "eth_requestAccounts",
+				});
+				// console.log('MetaMask accounts', accounts);
+				setWalletAddress(accounts[0]);
+
+				// Save the wallet address in local storage
+				localStorage.setItem("walletAddress", accounts[0]);
+
+				// Redirect to mint page if wallet is connected
+				if (accounts.length > 0) {
+					localStorage.setItem("tokenBalance", tokenBalance.toString());
+					router.push("/");
+				} else {
+					console.log("No accounts found.");
+				}
+			} catch (err: any) {
+				if (err.code === 4001) {
+					// EIP-1193 userRejectedRequest error.
+					console.log("Please connect to MetaMask.");
+				} else {
+					console.error(err);
+				}
+			}
+		} else {
+			console.log("MetaMask not detected");
+			alert("Please install MetaMask and connect to a network");
+		}
+	};
+
+	// const connectWallet = () => {
+	//   // Wallet connection logic goes here
+	// };
+
+	const handleContribution = async () => {
+		// Contribution handling logic goes here
+
+		try {
+			const { ethereum } = window;
+			if (walletAddress) {
+				const provider = new ethers.BrowserProvider(ethereum);
+				const signer = await provider.getSigner();
+				const tokenContract = new ethers.Contract(process.env.NEXT_PUBLIC_CAUSEKOIN_CONTRACT_ADDRESS as string, CAUSEKOIN_CONTRACT_ABI, signer);
+				try {
+					const balance = await tokenContract.claimTokens({ gasLimit: 300000 });
+					console.log("Reward claimed successfully:", balance);
+				} catch (error) {
+					console.error("Error claiming reward:", error);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
+			<main className="flex flex-col items-center gap-8 bg-white p-10 rounded-lg shadow-md">
+				<Image
+					src="/images/causekoin.png" // Updated path to images folder
+					alt="Your Project Logo"
+					width={180}
+					height={38}
+					priority
+				/>
+				<div className="flex flex-col items-center gap-4">
+					{!walletAddress ? (
+						<button
+							className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
+							// onClick={connectWallet}
+							onClick={requestAccount}>
+							Connect Wallet
+						</button>
+					) : (
+						<div className="text-center">
+							<p className="text-lg font-semibold">Connected Account:</p>
+							<p className="text-sm text-gray-600">{walletAddress}</p>
+							<p className="mt-2 text-lg font-semibold">Token Balance:</p>
+							<p className="text-sm text-gray-600">{tokenBalance} Tokens</p>
+						</div>
+					)}
+
+					<button
+						className={`px-6 py-3 bg-green-500 text-white rounded-full hover:bg-green-600 transition 
+              // !currentAccount ? "opacity-50 cursor-not-allowed" : ""
+            `}
+						onClick={handleContribution}
+						// disabled={!currentAccount}
+					>
+						I Have Contributed
+					</button>
+					<button
+						className="px-6 py-3 bg-green-500 text-white rounded-full hover:bg-green-600 transition"
+						onClick={() => getTokenBalance()}
+						// disabled={!currentAccount}
+					>
+						{tokenBalance}
+					</button>
+				</div>
+
+				<ol className="list-decimal list-inside text-left text-sm text-gray-700">
+					<li>Connect your wallet using the Connect Wallet button.</li>
+					<li>Indicate your contribution by clicking the I Have Contributed button.</li>
+					<li>Receive 100 tokens for every 10 contributions.</li>
+				</ol>
+			</main>
+
+			<footer className="mt-10 text-center text-gray-600">© {new Date().getFullYear()} Your Project Name. All rights reserved.</footer>
+		</div>
+	);
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,185 +1,151 @@
-// import { useState } from "react";
-"use client"
-import React, {useState} from 'react'
+import React from "react";
+import { Recycle, Coins, Award, ArrowRightCircle, Users, Target, Clock, Earth } from "lucide-react";
+import Link from "next/link";
 import Image from "next/image";
-import Web3 from 'web3';
-import { useRouter } from "next/navigation";
-import {ethers} from 'ethers';
 
-import {TOKEN_CONTRACT_ABI} from '@/config/TokenContract';
-import {CAUSEKOIN_CONTRACT_ABI} from '@/config/CauseKoin';
-// import * as dotenv from "dotenv";
-// dotenv.config();
+const LandingPage = () => {
+	return (
+		<div className="min-h-screen ">
+			<header className="bg-white shadow-md sticky top-0 z-50">
+				<nav className="container mx-auto px-6 py-3">
+					<div className="flex items-center justify-between">
+						<Image
+							src="/images/ecoCoinLogo.jpeg"
+							alt="EcoCoin Logo"
+							width={150}
+							height={150}
+						/>
+						<div className="hidden md:flex space-x-4">
+							<a
+								href="#how-it-works"
+								className="text-gray-600 hover:text-green-500">
+								How It Works
+							</a>
+							<a
+								href="#benefits"
+								className="text-gray-600 hover:text-green-500">
+								Benefits
+							</a>
+							<a
+								href="#levels"
+								className="text-gray-600 hover:text-green-500">
+								Level Up
+							</a>
+							<a
+								href="#claim"
+								className="text-gray-600 hover:text-green-500">
+								Claim Rewards
+							</a>
+						</div>
+					</div>
+				</nav>
+			</header>
 
+			<main className=" py-8">
+				<section className="container mx-auto text-center mb-16 h-screen flex flex-col justify-center items-center px-6">
+					<h1 className="text-4xl md:text-8xl font-bold text-gray-800 mb-4">Turn Plastic into Prosperity</h1>
+					<p className="text-xl text-gray-600 mb-8">Claim your EcoCoin rewards, level up, and amplify your impact. The more you recycle, the more you earn!</p>
+					<Link
+						href="/contribute"
+						className="bg-green-500 rounded-full text-white px-6 py-3 text-lg font-semibold hover:bg-green-600 transition duration-300">
+						Start Your Green Journey
+					</Link>
+					<Coins className="w-20 h-20 text-yellow-500 absolute top-40 right-40" />
+					<Recycle className="w-20 h-20 text-green-500 absolute top-40 left-40" />
+					<Earth className="w-9 h-9 text-blue-500 absolute top-[403px] right-[730px]" />
+				</section>
 
-export default function Home() {
-  // const [currentAccount, setCurrentAccount] = useState<string | null>(null);
-  const [tokenBalance, setTokenBalance] = useState<number>(0);
-  const [walletAddress, setWalletAddress] = useState<string>('');
-  const router = useRouter();
+				<section
+					id="how-it-works"
+					className="-mt-[100px] bg-green-200 py-20">
+					<div className="container mx-auto">
+						<h2 className="text-3xl font-bold text-green-500 mb-4">Turning Trash into Treasure</h2>
+						<p className="text-gray-600">At EcoCoin, we've gamified recycling. Collect plastic waste, bring it to our collection points, and watch as it transforms into valuable EcoCoin tokens. Each collection is minted as a unique NFT, proving your impact and earning you rewards. But that's not all – consistently great performance unlocks new levels, multiplying your earning potential. It's not just about cleaning up; it's about growing your impact and rewards over time.</p>
+					</div>
+				</section>
 
-  // Initialize Web3 instance
-  const web3 = window.ethereum ? new Web3(window.ethereum) : null;
+				<section
+					id="benefits"
+					className="pb-16 bg-green-200">
+					<div className="container mx-auto">
+						<h2 className="text-3xl font-bold text-gray-800 mb-8">Reap the Rewards of Responsibility</h2>
+						<div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+							<FeatureCard
+								icon={<Recycle className="w-8 h-8 text-green-500" />}
+								title="Eco-Friendly"
+								description="Collect plastic waste and make a real impact on the environment."
+							/>
+							<FeatureCard
+								icon={<Coins className="w-8 h-8 text-yellow-500" />}
+								title="Earn Rewards"
+								description="Get EcoCoin tokens for your recycling efforts."
+							/>
+							<FeatureCard
+								icon={<Award className="w-8 h-8 text-blue-500" />}
+								title="Unique NFTs"
+								description="Each collection is minted as a unique, verifiable NFT."
+							/>
+							<FeatureCard
+								icon={<Clock className="w-8 h-8 text-purple-500" />}
+								title="Auto-deploy"
+								description="Rewards are automatically deployed when conditions are met."
+							/>
+							<FeatureCard
+								icon={<Users className="w-8 h-8 text-indigo-500" />}
+								title="Community Driven"
+								description="Join a network of eco-conscious individuals making a difference."
+							/>
+							<FeatureCard
+								icon={<Target className="w-8 h-8 text-red-500" />}
+								title="Level Up"
+								description="Increase your impact and rewards as you progress through levels."
+							/>
+						</div>
+					</div>
+				</section>
 
-  const getTokenBalance = async () => {
-    try {
-      const { ethereum } = window;
-      if (walletAddress && ethereum) {
-        const provider = new ethers.BrowserProvider(ethereum);
-        const signer = await provider.getSigner();
-      
-        const tokenContract = new ethers.Contract(
-          process.env.NEXT_PUBLIC_CAUSEKOIN_CONTRACT_ADDRESS as string,
-          CAUSEKOIN_CONTRACT_ABI,
-          signer
-        );
+				<section
+					id="levels"
+					className="my-16 container mx-auto">
+					<h2 className="text-3xl font-bold text-gray-800 mb-4">Level Up, Earn More</h2>
+					<p className="text-gray-600 mb-6">Your commitment to cleaning our planet doesn't go unnoticed. Our level system rewards consistent action:</p>
+					<div className="bg-white p-6 rounded-lg shadow-md">
+						<h3 className="text-xl font-semibold text-gray-800 mb-4">How Leveling Works:</h3>
+						<ul className="list-disc list-inside text-gray-600 space-y-2">
+							<li>Start at Level 1 and earn base rewards for each plastic collection.</li>
+							<li>Mint Level 1 tokens four times to qualify for the Level 2 NFT.</li>
+							<li>Unlock Level 2 to boost your rewards and increase your impact.</li>
+							<li>Keep climbing! Higher levels mean greater rewards and recognition.</li>
+						</ul>
+					</div>
+				</section>
 
-        // Call contractTokenBalance method to get the token balance
-        const balance = await tokenContract.contractTokenBalance({ gasLimit: 300000 });
-        const formattedBalance = ethers.formatUnits(balance, 18); // Assuming 18 decimals for the token
-        console.log('Contract Token balance:', formattedBalance);
+				<section
+					id="claim"
+					className="text-center container mx-auto">
+					<h2 className="text-3xl font-bold text-gray-800 mb-4">Ready to Claim Your Impact?</h2>
+					<p className="text-gray-600 mb-8">You've done the hard work of collecting plastic and leveling up. Now it's time to reap the rewards. Connect your wallet, verify your collections, and watch your CauseKoin balance grow. Every token and level-up is a testament to your commitment to a cleaner Earth.</p>
+					<button className="bg-green-500 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-green-600 transition duration-300 flex items-center justify-center mx-auto">
+						Connect Wallet & Claim <ArrowRightCircle className="ml-2 w-5 h-5" />
+					</button>
+				</section>
+			</main>
 
-        setTokenBalance(parseFloat(formattedBalance));
-      } else {
-        console.log('No wallet address or Ethereum provider found');
-      }
-    } catch (error) {
-      console.log('Error fetching token balance:', error);
-    }
-  };
+			<footer className="bg-gray-800 text-white py-6">
+				<div className="container mx-auto px-6 text-center">
+					<p>&copy; 2024 EcoCoin. Empowering and rewarding environmental action through blockchain technology.</p>
+				</div>
+			</footer>
+		</div>
+	);
+};
 
+const FeatureCard = ({ icon, title, description }) => (
+	<div className="bg-white p-6 rounded-lg shadow-md">
+		<div className="flex justify-center mb-4">{icon}</div>
+		<h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
+		<p className="text-gray-600">{description}</p>
+	</div>
+);
 
-  const requestAccount = async (): Promise<void> => {
-    console.log('Requesting account...');
-    if (window.ethereum) {
-      try {
-        console.log('MetaMask detected');
-        const accounts: string[] = await window.ethereum.request({
-          method: 'eth_requestAccounts',
-        });
-        // console.log('MetaMask accounts', accounts);
-        setWalletAddress(accounts[0]);
-
-        // Save the wallet address in local storage
-        localStorage.setItem('walletAddress', accounts[0]);
-
-        // Redirect to mint page if wallet is connected
-        if (accounts.length > 0) {
-          localStorage.setItem('tokenBalance', tokenBalance.toString());
-          router.push('/');
-          
-        } else {
-          console.log('No accounts found.');
-        }
-      } catch (err: any) {
-        if (err.code === 4001) {
-          // EIP-1193 userRejectedRequest error.
-          console.log('Please connect to MetaMask.');
-        } else {
-          console.error(err);
-        }
-      }
-    } else {
-      console.log('MetaMask not detected');
-      alert('Please install MetaMask and connect to a network');
-    }
-  };
-
-  // const connectWallet = () => {
-  //   // Wallet connection logic goes here
-  // };
-
-  const handleContribution = async () => {
-    // Contribution handling logic goes here
-    
-      try {
-        const { ethereum } = window;
-        if (walletAddress) {
-          const provider = new ethers.BrowserProvider(ethereum);
-          const signer = await provider.getSigner();
-          const tokenContract = new ethers.Contract(
-            process.env.NEXT_PUBLIC_CAUSEKOIN_CONTRACT_ADDRESS as string,
-              CAUSEKOIN_CONTRACT_ABI,
-            signer
-          );
-          try {
-            const balance = await tokenContract.claimTokens({ gasLimit: 300000 });
-            console.log('Reward claimed successfully:', balance);
-          } catch (error) {
-            console.error('Error claiming reward:', error);
-          }
-
-        }
-      } catch (error) {
-        console.log(error);
-      }
-  
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
-      <main className="flex flex-col items-center gap-8 bg-white p-10 rounded-lg shadow-md">
-        <Image
-          src="/images/causekoin.png" // Updated path to images folder
-          alt="Your Project Logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div className="flex flex-col items-center gap-4">
-          {!walletAddress ? (
-            <button
-              className="px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
-              // onClick={connectWallet}
-              onClick={requestAccount}
-            >
-              Connect Wallet
-            </button>
-           ) : ( 
-            <div className="text-center">
-              <p className="text-lg font-semibold">Connected Account:</p>
-              <p className="text-sm text-gray-600">{walletAddress}</p>
-              <p className="mt-2 text-lg font-semibold">Token Balance:</p>
-              <p className="text-sm text-gray-600">{tokenBalance} Tokens</p>
-            </div>
-          )} 
-
-          <button
-            className={`px-6 py-3 bg-green-500 text-white rounded-full hover:bg-green-600 transition 
-              // !currentAccount ? "opacity-50 cursor-not-allowed" : ""
-            `}
-            onClick={handleContribution}
-            // disabled={!currentAccount}
-          >
-            I Have Contributed
-          </button>
-           <button
-            className="px-6 py-3 bg-green-500 text-white rounded-full hover:bg-green-600 transition"
-            onClick={() => getTokenBalance()}
-            // disabled={!currentAccount}
-          >
-            {tokenBalance}
-          </button>
-
-          
-        </div>
-
-        <ol className="list-decimal list-inside text-left text-sm text-gray-700">
-          <li>
-            Connect your wallet using the Connect Wallet button.
-          </li>
-          <li>
-            Indicate your contribution by clicking the I Have Contributed button.
-          </li>
-          <li>
-            Receive 100 tokens for every 10 contributions.
-          </li>
-        </ol>
-      </main>
-
-      <footer className="mt-10 text-center text-gray-600">
-        © {new Date().getFullYear()} Your Project Name. All rights reserved.
-      </footer>
-    </div>
-  );
-}
+export default LandingPage;
